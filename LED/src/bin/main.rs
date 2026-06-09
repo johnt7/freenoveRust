29659851115a0591d3cd9269fc84b7fbf3cecaac
John Turner <johnt7_@hotmail.com>
2026-06-09 19:04:01 -0400

    added led sample

---
+++ LED/src/bin/main.rs
@@ -0,0 +1,59 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::rmt::Rmt;
+use esp_hal::time::{Duration, Instant, Rate};
+use esp_hal_smartled::{SmartLedsAdapter, smart_led_buffer};
+use esp_backtrace as _;
+use esp_println::println;
+use smart_leds::{RGB8, SmartLedsWrite as _};
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    // generator version: 1.3.0
+    // generator parameters: --chip esp32s3 -o esp32s3-wroom-1 -o vscode -o esp
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    let frequency = Rate::from_mhz(80);
+    let rmt = Rmt::new(peripherals.RMT, frequency).expect("failed to initialize RMT");
+    let mut ws2812_buf = smart_led_buffer!(1);
+    let mut ws2812 = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO48, &mut ws2812_buf);
+
+    let colors = [
+        RGB8 { r: 32, g: 0, b: 0 },
+        RGB8 { r: 0, g: 32, b: 0 },
+        RGB8 { r: 0, g: 0, b: 32 },
+        RGB8 { r: 0, g: 0, b: 0 },
+    ];
+    let mut idx = 0usize;
+
+    println!("app started");
+
+    loop {
+        println!("loop");
+        ws2812.write([colors[idx]].into_iter()).unwrap();
+        idx = (idx + 1) % colors.len();
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
+}
